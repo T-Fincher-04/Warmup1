80c4edd13a80ad4c2f788bbf91b619ace058fdc4
T-Fincher-04 <tfincher1@student.cscc.edu>
2026-01-26 17:29:42 -0500

    Added the final part of the code to randomize and color each cube in the circle.

--- Warmup.py
+++ Warmup.py
@@ -9,6 +9,19 @@ class MyApp(ShowBase):
         self.fighter.reparentTo(self.render)
         self.fighter.setColorScale(1.0, 0.0, 0.0, 1.0)
         self.accept('escape', self.quit)
+        self.parent =self.loader.loadModel("./Assets/cube")
+
+        x = 0
+        for i in range(100):
+            theta = x
+            self.placeholder2 = self.render.attachNewNode('Placeholder2')
+            self.placeholder2.setPos(50.0 * math.cos(theta),50.0 * math.sin(theta), 0.0 * math.tan(theta))
+            red = 0.6 + random.random() * 0.4
+            green =  0.6 + random.random() * 0.4
+            blue =  0.6 + random.random() * 0.4
+            self.placeholder2.setColorScale(red, green, blue, 1.0)
+            self.parent.instanceTo(self.placeholder2)
+            x = x + 0.06
 
     def quit(self):
         sys.exit()
