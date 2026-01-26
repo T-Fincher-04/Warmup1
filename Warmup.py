42b331636ba23c2a7ea33a3c051cd912c35df71f
T-Fincher-04 <tfincher1@student.cscc.edu>
2026-01-26 16:30:43 -0500

    Set up Panda3D and added code for the model.

---
+++ Warmup.py
@@ -0,0 +1,15 @@
+from direct.showbase.ShowBase import ShowBase
+
+
+class MyApp(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.fighter = self.loader.loadModel('./Assets/sphere')
+        self.fighter.reparentTo(self.render)
+        self.fighter.setColorScale(1.0, 0.0, 0.0, 1.0)
+        
+
+app = MyApp()
+app.run()
+
