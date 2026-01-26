1df9c2234d31d4921d23a93ae975869504cecedb
T-Fincher-04 <tfincher1@student.cscc.edu>
2026-01-26 16:43:25 -0500

    Added a way to quit the program along with imports.

--- Warmup.py
+++ Warmup.py
@@ -8,7 +8,12 @@ class MyApp(ShowBase):
         self.fighter = self.loader.loadModel('./Assets/sphere')
         self.fighter.reparentTo(self.render)
         self.fighter.setColorScale(1.0, 0.0, 0.0, 1.0)
+        self.accept('escape', self.quit)
+
+    def quit(self):
+        sys.exit()
         
+import math, sys, random
 
 app = MyApp()
 app.run()
