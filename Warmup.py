35ed15a1ff8ae5334217caa8b59ae4ef9329482c
T-Fincher-04 <tfincher1@student.cscc.edu>
2026-02-09 15:20:23 -0500

    Player can now move in each direction.

--- Warmup.py
+++ Warmup.py
@@ -1,5 +1,7 @@
-from direct.showbase.ShowBase import ShowBase
+# Player Movement in 3D Space
 
+from direct.showbase.ShowBase import ShowBase
+import math, sys, random
 
 class MyApp(ShowBase):
 
@@ -9,22 +11,31 @@ class MyApp(ShowBase):
         self.fighter.reparentTo(self.render)
         self.fighter.setColorScale(1.0, 0.0, 0.0, 1.0)
         self.accept('escape', self.quit)
-        self.parent =self.loader.loadModel("./Assets/cube")
+        self.parent = self.loader.loadModel("./Assets/cube")
         self.disableMouse()
         self.camera.setPos(0.0, 0.0, 250.0)
         self.camera.setHpr(0.0, -90.0, 0.0)
-        self.accept('escape', self.quit)
-        self.accept('arrow left', self.negativeX,[1])
-        self.accept('arrow left-up', self.negativeX, [0])
+        self.accept('arrow_left', self.negativeX, [1])
+        self.accept('arrow_left-up', self.negativeX, [0])
+        self.accept('arrow_right', self.positiveX, [1])
+        self.accept('arrow_right-up', self.positiveX, [0])
+        self.accept('arrow_up', self.positiveY, [1])
+        self.accept('arrow_up-up', self.positiveY, [0])
+        self.accept('arrow_down', self.negativeY, [1])
+        self.accept('arrow_down-up', self.negativeY, [0])
+        self.accept('arrow_forward', self.positiveZ, [1])
+        self.accept('arrow_forward-up', self.positiveZ, [0])
+        self.accept('arrow_backward', self.negativeZ, [1])
+        self.accept('arrow_backward-up', self.negativeZ, [0])
 
         x = 0
         for i in range(100):
             theta = x
             self.placeholder2 = self.render.attachNewNode('Placeholder2')
-            self.placeholder2.setPos(50.0 * math.cos(theta),50.0 * math.sin(theta), 0.0 * math.tan(theta))
+            self.placeholder2.setPos(50.0 * math.cos(theta), 50.0 * math.sin(theta), 0.0 * math.tan(theta))
             red = 0.6 + random.random() * 0.4
-            green =  0.6 + random.random() * 0.4
-            blue =  0.6 + random.random() * 0.4
+            green = 0.6 + random.random() * 0.4
+            blue = 0.6 + random.random() * 0.4
             self.placeholder2.setColorScale(red, green, blue, 1.0)
             self.parent.instanceTo(self.placeholder2)
             x = x + 0.06
@@ -33,18 +44,64 @@ class MyApp(ShowBase):
         sys.exit()
     
     def negativeX(self, keyDown):
-        if (keyDown):
+        if keyDown:
             self.taskMgr.add(self.moveNegativeX, 'moveNegativeX')
         else:
             self.taskMgr.remove('moveNegativeX')
 
+    def positiveX(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.movePositiveX, 'movePositiveX')
+        else:
+            self.taskMgr.remove('movePositiveX')
+
+    def negativeY(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.moveNegativeY, 'moveNegativeY')
+        else:
+            self.taskMgr.remove('moveNegativeY')
+
+    def positiveY(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.movePositiveY, 'movePositiveY')
+        else:
+            self.taskMgr.remove('movePositiveY')
+
+    def negativeZ(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.moveNegativeZ, 'moveNegativeZ')
+        else:
+            self.taskMgr.remove('moveNegativeZ')
+
+    def positiveZ(self, keyDown):
+        if keyDown:
+            self.taskMgr.add(self.movePositiveZ, 'movePositiveZ')
+        else:
+            self.taskMgr.remove('movePositiveZ')
+
     def moveNegativeX(self, task):
         self.fighter.setX(self.fighter, -1)
         return task.cont
-        
-import math, sys, random
 
+    def movePositiveX(self, task):
+        self.fighter.setX(self.fighter, 1)
+        return task.cont
 
+    def moveNegativeY(self, task):
+        self.fighter.setY(self.fighter, -1)
+        return task.cont
+
+    def movePositiveY(self, task):
+        self.fighter.setY(self.fighter, 1)
+        return task.cont
+
+    def moveNegativeZ(self, task):
+        self.fighter.setZ(self.fighter, -1)
+        return task.cont
+
+    def movePositiveZ(self, task):
+        self.fighter.setZ(self.fighter, 1)
+        return task.cont
 
 app = MyApp()
 app.run()
